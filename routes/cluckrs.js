53b7de2a48ac5e178c242a6d593244feae92c4f3
kentcheung2000 <kentcheung2000@yahoo.com>
2019-08-27 20:02:11 -0700

    Fix index page

--- routes/cluckrs.js
+++ routes/cluckrs.js
@@ -9,7 +9,13 @@ const router = express.Router();
 // router.use('/cluckr', cluckr);
 
 router.get("/", (req, res) => {
-    res.render('index');
+    // res.render('index');
+    knex.select('*').from('cluckr')
+        .then(data => {
+            res.render("index", {
+                cluckr: data
+            });
+        })
 })
 
 const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24 * 7;
@@ -18,7 +24,7 @@ router.post("/sign_in", (req, res) => {
     res.cookie('username', req.body.username, {
         maxAge: new Date(COOKIE_MAX_AGE)
     })
-    res.redirect('/');
+    res.redirect('/new');
 })
 
 router.post("/sign_out", (req, res) => {
